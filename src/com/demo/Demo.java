49ca9d1cef02619d58141d30023da3c4f0f9c398
Sonawane <kartik.sonawane@capgemini.com>
2024-03-13 14:33:29 +0530

    2nd commit

--- src/com/demo/Demo.java
+++ src/com/demo/Demo.java
@@ -3,7 +3,10 @@ package com.demo;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("this is demo project for git ..");
+		
+		String s = "kartik sonawane";
+		System.out.println(s);
 
 	}
 
